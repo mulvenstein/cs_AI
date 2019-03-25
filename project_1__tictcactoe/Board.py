bdc5628d3809e00e0813ceafd50257bd07475968
Tom Mulvey <tommulvey5567@gmail.com>
2019-03-25 17:56:48 -0400

    added functionality of player_at method in board class

--- project_1__tictcactoe/Board.py
+++ project_1__tictcactoe/Board.py
@@ -11,7 +11,7 @@ class board:
           6 | 7 | 8
     ''' 
     def __init__(self):
-        self.board = [ '_' for _ in range(9) ] # inits board of 9 '_',
+        self.board = [ '█' for _ in range(9) ] # inits board of 9 '_',
         self.turns_played = 0 #9 max.
 
     '''
@@ -19,47 +19,47 @@ class board:
     returns TRUE if player won, FALSE otherwise
     '''
     def is_winner(self, player): 
-       return False
+        return False
 
     '''
     returns TRUE if board is full and neither player has won
     '''
     def is_tie(self):
-       return False
+        return False
 
     '''
     returns TRUE if board is full (9 valid turns)
     '''
     def is_board_full(self):  
-       return False
+        return False
 
     '''
     given move to row,col , is it in the playing area?
     returns TRUE if yes, FALSE otherwise
     '''
     def is_valid_move(self, move):  
-       return False
+        return False
 
     '''
     returns how many valid moves have been played
     '''
     def num_turns(self): 
-       return False
+        return False
 
     '''
     given position in board, return 'x', 'o', or '_'
     '''
     def player_at(self, position):
-       return False
+        return self.board[position]
     
     '''
     given player 'x' or 'o' and a move, make it!
     returns TRUE if valid, FALSE otherwise
     '''
     def make_move(self, player, move): 
-       if(is_valid_move(move)):
-           self.board[move] = player
-           return True
+        if(is_valid_move(move)):
+            self.board[move] = player
+            return True
         return False
 
     '''
